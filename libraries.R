726a38e768a27b1e56ffeece37666b23e95fd300
yleniarotalinti <56341110+yleniarotalinti@users.noreply.github.com>
2022-07-27 14:46:07 +0100

    Add files via upload

---
+++ libraries.R
@@ -0,0 +1,46 @@
+#libraries
+
+#install.packages("tidyverse")
+
+#The core tidyverse includes the packages that you're likely to use in everyday data analyses 
+#It contains: ggplot2, dplyr, tidyr, readr, purrr, tibble, stringr, forcats
+library(tidyverse)
+
+#library to read excels
+library(readxl)
+#library to implement naiveBayes model, randomForest, neuralnetworks
+library(e1071)
+library(randomForest)
+library(torch)
+
+#library to compute confusionMatrix
+library(caret)
+
+#library to compute ROC and PR curve
+library(precrec)
+library(pROC)
+
+#library with mixedorderfunction
+library(gtools)
+
+#library to color the plots
+library(viridis)
+
+#library to write excel files
+library(openxlsx)
+
+#library to hadle colorful outputs in console
+library(crayon)
+
+#library to compute the last day of a month
+library(lubridate)
+
+#library to compute subplots
+library(ggpubr)
+
+#library to compute Cramer's V matrix between categorical variables
+library(creditmodel)
+
+#library to compute Tetrachoric Correlation between binary variables
+library(psych)
+
